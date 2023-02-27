43e244ab1027a5c7034fbb8c156317bc9d2a58f9
Urvi Pandit <95270207+Urvi-Pandit@users.noreply.github.com>
2023-02-28 00:22:37 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', evt => {
+    console.log('service worker has been installed');
+});
+
+self.addEventListener('activate', evt => {
+    console.log('service worker has been activated');
+});
+
+self.addEventListener('fetch', evt => {
+    console.log('fetch event', evt);
+});
+
+self.addEventListener('sync', evt => {
+    console.log('sync event', evt);
+});
