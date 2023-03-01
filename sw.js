ab05e1a2cdba0381d2926786939b96deffa2064a
Urvi Pandit <95270207+Urvi-Pandit@users.noreply.github.com>
2023-03-01 12:26:59 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -34,3 +34,58 @@ self.addEventListener('push', (event) => {
       self.registration.showNotification(title, options)
     )
 });
+self.addEventListener("push", function (event) {
+  if (event && event.data) {
+    var data = event.data.json();
+    if (data.method == "pushMessage") {
+      console.log("Push notification sent");
+      event.waitUntil(
+        self.registration.showNotification("Urvi Resume", {
+          body: data.message,
+          icon: "/cropped.png",
+        })
+      );
+    }
+  }
+});
+
+var filesToCache = ["/profile/index.html"];
+
+var preLoad = function () {
+  return caches.open("offline").then(function (cache) {
+    // caching index and important routes
+    return cache.addAll(filesToCache);
+  });
+};
+
+var checkResponse = function (request) {
+  return new Promise(function (fulfill, reject) {
+    fetch(request).then(function (response) {
+      if (response.status !== 404) {
+        fulfill(response);
+      } else {
+        reject();
+      }
+    }, reject);
+  });
+};
+
+var addToCache = function (request) {
+  return caches.open("offline").then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response);
+    });
+  });
+};
+
+var returnFromCache = function (request) {
+  return caches.open("offline").then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      if (!matching || matching.status == 404) {
+        return cache.match("offline.html");
+      } else {
+        return matching;
+      }
+    });
+  });
+};
